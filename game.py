8de818e0f47e46b0443d0f53a346826eccf70c79
Meet Maratha <meet.maratha6@gmail.com>
2022-06-04 18:44:37 +0530

    simple file

---
+++ game.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pygame
+
+class Snake:
+    def __init__(self):
+        self.p = "Hello"
+    
+    def prin(self):
+        print(self.p)
+
+    
+if __name__ == "__main__":
+    s = Snake()
+    s.prin()
